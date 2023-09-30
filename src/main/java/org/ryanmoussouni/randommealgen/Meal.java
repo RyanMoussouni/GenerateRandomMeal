284ba4c49a5d5c198fffd50cd64e541007ba8de6
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-09-30 21:30:54 +0200

    Add Meal

--- src/main/java/org/ryanmoussouni/randommealgen/Meal.java
+++ src/main/java/org/ryanmoussouni/randommealgen/Meal.java
@@ -1,2 +1,113 @@
-package org.ryanmoussouni.randommealgen;public class Meal {
+package org.ryanmoussouni.randommealgen;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Meal {
+    @JsonProperty
+    private String idMeal;
+    @JsonProperty
+    private String strMeal;
+    @JsonProperty
+    private String strDrinkAlternate;
+    @JsonProperty
+    private String strCategory;
+    @JsonProperty
+    private String strArea;
+    @JsonProperty
+    private String strInstructions;
+    @JsonProperty
+    private String strMealThumb;
+    @JsonProperty
+    private String strTags;
+    @JsonProperty
+    private String strYoutube;
+    @JsonProperty
+    private String strIngredient1;
+    @JsonProperty
+    private String strIngredient2;
+    @JsonProperty
+    private String strIngredient3;
+    @JsonProperty
+    private String strIngredient4;
+    @JsonProperty
+    private String strIngredient5;
+    @JsonProperty
+    private String strIngredient6;
+    @JsonProperty
+    private String strIngredient7;
+    @JsonProperty
+    private String strIngredient8;
+    @JsonProperty
+    private String strIngredient9;
+    @JsonProperty
+    private String strIngredient10;
+    @JsonProperty
+    private String strIngredient11;
+    @JsonProperty
+    private String strIngredient12;
+    @JsonProperty
+    private String strIngredient13;
+    @JsonProperty
+    private String strIngredient14;
+    @JsonProperty
+    private String strIngredient15;
+    @JsonProperty
+    private String strIngredient16;
+    @JsonProperty
+    private String strIngredient17;
+    @JsonProperty
+    private String strIngredient18;
+    @JsonProperty
+    private String strIngredient19;
+    @JsonProperty
+    private String strIngredient20;
+    @JsonProperty
+    private String strMeasure1;
+    @JsonProperty
+    private String strMeasure2;
+    @JsonProperty
+    private String strMeasure3;
+    @JsonProperty
+    private String strMeasure4;
+    @JsonProperty
+    private String strMeasure5;
+    @JsonProperty
+    private String strMeasure6;
+    @JsonProperty
+    private String strMeasure7;
+    @JsonProperty
+    private String strMeasure8;
+    @JsonProperty
+    private String strMeasure9;
+    @JsonProperty
+    private String strMeasure10;
+    @JsonProperty
+    private String strMeasure11;
+    @JsonProperty
+    private String strMeasure12;
+    @JsonProperty
+    private String strMeasure13;
+    @JsonProperty
+    private String strMeasure14;
+    @JsonProperty
+    private String strMeasure15;
+    @JsonProperty
+    private String strMeasure16;
+    @JsonProperty
+    private String strMeasure17;
+    @JsonProperty
+    private String strMeasure18;
+    @JsonProperty
+    private String strMeasure19;
+    @JsonProperty
+    private String strMeasure20;
+    @JsonProperty
+    private String strSource;
+    @JsonProperty
+    private String strImageSource;
+    @JsonProperty
+    private String strCreativeCommonsConfirmed;
+    @JsonProperty
+    private String dateModified;
 }
