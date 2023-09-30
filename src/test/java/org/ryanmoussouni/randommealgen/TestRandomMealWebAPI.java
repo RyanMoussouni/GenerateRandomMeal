c2d9e694475ca4b1d35444bd7eba83e592d23716
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-09-30 17:19:41 +0200

    Add performHttpRequest integration test to web api

--- src/test/java/org/ryanmoussouni/randommealgen/TestRandomMealWebAPI.java
+++ src/test/java/org/ryanmoussouni/randommealgen/TestRandomMealWebAPI.java
@@ -3,24 +3,32 @@ package org.ryanmoussouni.randommealgen;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class TestRandomMealWebAPI {
     public static final String MEAL_WEB_API_ENDPOINT_URI = "www.themealdb.com/api/json/v1/1/random.php";
-    private HttpRequest apiClient;
+    private HttpClient apiClient;
 
     @BeforeEach
-    void setUp() throws URISyntaxException {
-        apiClient = HttpRequest.newBuilder()
-                .uri(new URI(MEAL_WEB_API_ENDPOINT_URI))
-                .GET()
+    void setUp() {
+        apiClient = HttpClient.newBuilder()
                 .build();
     }
 
     @Test
-    void performHttpRequest_returnsListOfMeal() {
+    void performHttpRequest_returnsListOfMeal() throws IOException, InterruptedException, URISyntaxException {
+        var httpRequest = HttpRequest.newBuilder()
+                .uri(new URI(MEAL_WEB_API_ENDPOINT_URI))
+                .GET()
+                .build();
+
+        apiClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
         throw new RuntimeException();
     }
 }
