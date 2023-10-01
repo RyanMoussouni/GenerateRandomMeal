701349d2ea3ecea29392b710aea98250d84dab59
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-01 15:42:48 +0200

    Add getMeals test -> error when fetching

--- src/test/java/org/ryanmoussouni/randommealgen/business/RandomMealWebApiAdapterTest.java
+++ src/test/java/org/ryanmoussouni/randommealgen/business/RandomMealWebApiAdapterTest.java
@@ -3,16 +3,22 @@ package org.ryanmoussouni.randommealgen.business;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.ryanmoussouni.randommealgen.error.MealGenerationError;
+import org.ryanmoussouni.randommealgen.error.WebCommunicationException;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class RandomMealWebApiAdapterTest {
     @Test
-    void getMeals_errorWhenFetchingFromApi_throwsMealGenerationError() {
-        var exception = buidMealGenerationError();
+    void getMeals_errorWhenFetching_throwsMealGenerationError() {
         var mealFetcherMock = mock(MealFetcher.class);
-        when(mealFetcherMock).thenThrow(exception);
+        try {
+            doAnswer((invocation) -> {
+                throw new MealGenerationError(new Exception());
+            }).when(mealFetcherMock)
+            .fetchMeals();
+        } catch (WebCommunicationException e) {
+            // do nothing
+        }
         var adapter = new RandomMealWebApiAdapter();
         adapter.setHttpClient(mealFetcherMock);
 
