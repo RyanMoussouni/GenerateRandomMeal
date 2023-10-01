244ffa81798c7bd55a266eb5e230ce5a48d88edd
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-01 16:34:08 +0200

    Add getMealsSingleMealReturnedByFetcher

--- src/test/java/org/ryanmoussouni/randommealgen/business/RandomMealWebApiAdapterTest.java
+++ src/test/java/org/ryanmoussouni/randommealgen/business/RandomMealWebApiAdapterTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.ryanmoussouni.randommealgen.error.MealGenerationError;
 import org.ryanmoussouni.randommealgen.error.WebCommunicationException;
 
+import java.net.http.HttpResponse;
+
 import static org.mockito.Mockito.*;
 
 class RandomMealWebApiAdapterTest {
@@ -30,8 +32,89 @@ class RandomMealWebApiAdapterTest {
         }
     }
 
-    private MealGenerationError buidMealGenerationError() {
-        return new MealGenerationError(new Exception());
+    @Test
+    void getMeals_singleMealReturnedByFetcher_() {
+        var mealFetcherStub = mock(MealFetcher.class);
+        var httpResponseStub = mock(HttpResponse.class);
+        when(httpResponseStub.body())
+                .thenReturn(createMealFixture());
+        try {
+            when(mealFetcherStub.fetchMeals())
+                    .thenReturn(httpResponseStub);
+        } catch (WebCommunicationException e) {
+            //do nothing
+        }
+        var adapter = new RandomMealWebApiAdapter();
+        adapter.setHttpClient(mealFetcherStub);
+
+        try {
+            adapter.getMeal();
+        } catch (MealGenerationError e) {
+            Assertions.fail();
+        }
     }
 
+    private String createMealFixture() {
+        return """
+                {
+                    "meals": [
+                        {
+                            "idMeal": "53076",
+                            "strMeal": "Bread omelette",
+                            "strDrinkAlternate": null,
+                            "strCategory": "Breakfast",
+                            "strArea": "Indian",
+                            "strInstructions": "Make and enjoy",
+                            "strMealThumb": "https://www.themealdb.com/images/media/meals/hqaejl1695738653.jpg",
+                            "strTags": null,
+                            "strYoutube": "",
+                            "strIngredient1": "Bread",
+                            "strIngredient2": "Egg",
+                            "strIngredient3": "Salt",
+                            "strIngredient4": "",
+                            "strIngredient5": "",
+                            "strIngredient6": "",
+                            "strIngredient7": "",
+                            "strIngredient8": "",
+                            "strIngredient9": "",
+                            "strIngredient10": "",
+                            "strIngredient11": "",
+                            "strIngredient12": "",
+                            "strIngredient13": "",
+                            "strIngredient14": "",
+                            "strIngredient15": "",
+                            "strIngredient16": "",
+                            "strIngredient17": "",
+                            "strIngredient18": "",
+                            "strIngredient19": "",
+                            "strIngredient20": "",
+                            "strMeasure1": "2",
+                            "strMeasure2": "2",
+                            "strMeasure3": "0.5",
+                            "strMeasure4": " ",
+                            "strMeasure5": " ",
+                            "strMeasure6": " ",
+                            "strMeasure7": " ",
+                            "strMeasure8": " ",
+                            "strMeasure9": " ",
+                            "strMeasure10": " ",
+                            "strMeasure11": " ",
+                            "strMeasure12": " ",
+                            "strMeasure13": " ",
+                            "strMeasure14": " ",
+                            "strMeasure15": " ",
+                            "strMeasure16": " ",
+                            "strMeasure17": " ",
+                            "strMeasure18": " ",
+                            "strMeasure19": " ",
+                            "strMeasure20": " ",
+                            "strSource": "",
+                            "strImageSource": null,
+                            "strCreativeCommonsConfirmed": null,
+                            "dateModified": null
+                        }
+                    ]
+                }
+                """;
+    }
 }
